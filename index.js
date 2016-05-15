0973367edbabe6629771bd04d192a3e697352799
Damien Maillard <dmaillard06@gmail.com>
2016-05-15 17:24:15 +0200

    update to jsenv, test needs a rewrite

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import DuplexStream from './lib/stream-duplex.js';
 import TransformStream from './lib/stream-transform.js';
 
 export default {
-	Duplex: DuplexStream,
-	Transform: TransformStream
+    Duplex: DuplexStream,
+    Transform: TransformStream
 };
